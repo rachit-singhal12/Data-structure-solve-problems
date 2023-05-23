03836e36f794de9795799d9f73c4a1dd9f93602a
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-05-23 20:47:59 +0530

    Add files via upload

---
+++ sorting/bubble_sort.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {3,2,8,5,6,7};
+    for(int i=0;i<6;i++)
+    {
+        for(int j=0;j<6;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    for(int i=0;i<6;i++)cout<<arr[i]<<" ";
+    return 0;
+}
