229883375928a53e186202e41f9265c22fc612e0
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-07-02 20:04:08 +0530

    Update bubble_sort.cpp

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -6,7 +6,7 @@ int main()
     int arr[] = {3,2,8,5,6,7};
     for(int i=0;i<6;i++)
     {
-        for(int j=0;j<6;j++)
+        for(int j=0;j<5-i-1;j++)
         {
             if(arr[j]>arr[j+1])
             {
