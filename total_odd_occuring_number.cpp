cfcdc3a535161930c5995453ff9ae5611b3a856f
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2023-08-20 14:01:22 +0530

    Add files via upload

---
+++ total_odd_occuring_number.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,flag=0;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)cin>>arr[i];
+    for(int i=0;i<n;i++)
+    {
+        int temp=0;
+        for(int j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            temp++;
+        }
+        if((temp&1)==1)
+        {
+            cout<<arr[i]<<" ";
+        }
+    }
+    cout<<flag;
+    return 0;
+}
